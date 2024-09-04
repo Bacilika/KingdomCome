9eb2fa664c6fe0e4abcb2381917c3f2829a90e70
Bacilika Glansholm <lunasotiz@gmail.com>
2024-09-04 15:32:07 +0200

    initial commit

---
+++ Scripts/Constants.cs
@@ -0,0 +1,26 @@
+namespace Scripts.Constants;
+
+public static class Constants
+{
+	public const string ViewPortHeightSettingPath = "display/window/size/viewport_height";
+	public const string ViewPortWidthSettingPath = "display/window/size/viewport_width";
+}
+
+public static class Inputs
+{
+	public const string LeftClick = "left_click";
+	public const string ClearCursor = "clear_cursor";
+	public const string ZoomIn = "zoom_in";
+	public const string ZoomOut = "zoom_out";
+	public const string CameraPan = "camera_pan";
+	public const string CameraUp = "camera_up"
+	public const string CameraDown = "camera_down"
+	public const string CameraLeft = "camera_left"
+	public const string CameraRight = "camera_right"
+}
+
+public static class Signals
+{
+	public const string Pressed = "pressed";
+	public const string ItemSelected = "item_selected";
+}

c80895e198690713e5aa58c20a5e18b872e5f53d
uttu <jorunn.helena@gmail.com>
2024-10-02 21:48:23 +0200

    Added changes for tavern, upgrade tavern png

--- Scripts/Building/Activities/Tavern.cs
+++ Scripts/Building/Activities/Tavern.cs
@@ -1,39 +1,24 @@
 using Godot;
 using System;
 using System.Collections.Generic;
+using KingdomCome.Scripts.Building.Activities;
 using Scripts.Constants;
 
-public partial class Tavern : AbstractPlaceable
+public partial class Tavern : AbstractActivity
 {
 	
-	protected override void Tick()
-	{
-
-	}
-	
-	protected override void OnDelete()
-	{
-		GameLogistics.Resources["Wood"] += Upgrades[Upgrade.WoodBackOnDelete][Level];
-		GameLogistics.Resources["Stone"] += Upgrades[Upgrade.StoneBackOnDelete][Level];
-		Shop.deleteAudio.Play();
-		QueueFree();
-	}
 	
 	public override void _Ready_instance()
 	{
-		Console.WriteLine("Church instantiated");
+		BuildingName = "Tavern";
+		BuildingDescription = "A place for citizens to gather and drink beer. Requires water and foot";
 		Price = 5000;
 		Upgrades = new Dictionary<string, List<int>>
 		{
-			{ Upgrade.Cost, [5000, 3000, 3000] }, 
-			{ Upgrade.MaxInhabitants, [5, 7, 10] },
-			{ Upgrade.Workers, [5, 7, 10] },
-			{ Upgrade.WoodCost, [5, 7, 10] },
-			{ Upgrade.StoneCost, [5, 7, 10]}, 
-			{Upgrade.MoneyBackOnDelete, [4000, 2000, 2000] },
-			{Upgrade.WoodBackOnDelete, [3, 7, 15]},
-			{Upgrade.StoneBackOnDelete, [3, 7, 15]}, 
-			{Upgrade.WoodMoveCost, [2, 5, 10]}, 
+			{Upgrade.Cost, [5000, 3000, 3000]}, {Upgrade.MaxWorkers, [3, 5, 7]},
+			{Upgrade.Inhabitants, [5, 7, 10]}, {Upgrade.WoodCost, [2, 2, 2]},
+			{Upgrade.StoneCost, [2, 2, 2]}, {Upgrade.MoneyBackOnDelete, [4000, 2000, 2000] },{Upgrade.WoodBackOnDelete, [3, 7, 15]},
+			{Upgrade.StoneBackOnDelete, [3, 7, 15]}, {Upgrade.WoodMoveCost, [2, 5, 10]},
 			{Upgrade.StoneMoveCost, [2, 5, 10]}
 		};
 	}
